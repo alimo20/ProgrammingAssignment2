746415c62017dce5776dfc0305ebca66c0792e38
Rooster Illusion <alimo20@gmail.com>
2017-01-10 17:53:01 -0500

    get and set methods working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains 2 fuctions, one to create a cache matrix, the other to create solved
+## inverse matrices
 
-## Write a short comment describing this function
+## makeCacheMatrix contains a list of 4 list values that contain getter/setter functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #get matrix
+  #set matrix
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  
+  #set inverse
+  #get inverse
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cachesolve checks to see if a pre-cached version exists for a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+      ## Return a matrix that is the inverse of 'x'
+      ## search the cache
+      ## if found, return that
+      ## if not, cache and return the obect
 }
